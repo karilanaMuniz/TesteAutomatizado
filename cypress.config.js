b3c4809fe41405b05c6ebfb415ad64a5ce6f29b4
Karilana Santos <karilana.santos@ucsal.edu.br>
2024-05-28 11:41:01 -0300

    Usando allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
   projectId: 'xbob95',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureCypress(on);
     },
     video: true,
-    baseUrl: 'https://front.serverest.dev/'
+    baseUrl: 'https://front.serverest.dev/',
+
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+import "allure-cypress/commands";
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
